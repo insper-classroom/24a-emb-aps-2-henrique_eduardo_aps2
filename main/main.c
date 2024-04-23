27270f7e71019c314e3dcfe964d51f638fb54073
HenriqueFBadin <henrique.badin@gmail.com>
2024-04-23 00:21:46 +0000

    lógica nova

--- main/main.c
+++ main/main.c
@@ -10,11 +10,11 @@
 #include "hardware/adc.h"
 #include <stdio.h>
 #include "hc06.h"
-#define A_button 11
-#define S_button 12
+#define A_button 15
+#define S_button 14
 #define J_button 13
-#define K_button 14
-#define L_button 15
+#define K_button 12
+#define L_button 11
 #define ADC_y 26
 #define ADC_y_ID 0
 #define ADC_Sound 27
@@ -23,96 +23,42 @@
 QueueHandle_t xQueueBTN;
 SemaphoreHandle_t xSemaphore_r;
 
-typedef struct {
-    int button;
-    int level;
-} data;
-
+volatile char byteArray = 0b00000000;
 void btn_callback(uint gpio, uint32_t events){
     int button_pressed;
     if(events == GPIO_IRQ_EDGE_FALL){
         if(gpio == A_button){
-            button_pressed=1;
-            data d;
-            d.button = 1;
-            d.level = 1;
-            
-            xQueueSendFromISR(xQueueBTN,&d,0);
+            byteArray = 0b00000001 | byteArray;
         }
         if(gpio == S_button){
-            button_pressed=2;
-            data d;
-            d.button = 2;
-            d.level = 1;
-            
-            xQueueSendFromISR(xQueueBTN,&d,0);
+            byteArray = 0b00000010 | byteArray;
         }
         if(gpio == J_button){
-            button_pressed=3;
-            data d;
-            d.button = 3;
-            d.level = 1;
-            
-            xQueueSendFromISR(xQueueBTN,&d,0);
+            byteArray = 0b00000100 | byteArray;
         }
         if(gpio == K_button){
-            button_pressed=4;
-            data d;
-            d.button = 4;
-            d.level = 1;
-            
-            xQueueSendFromISR(xQueueBTN,&d,0);
+            byteArray = 0b00001000 | byteArray;
         }
         if(gpio == L_button){
-            button_pressed=5;
-            data d;
-            d.button = 5;
-            d.level = 1;
-            
-            xQueueSendFromISR(xQueueBTN,&d,0);
+            byteArray = 0b00010000 | byteArray;
         }
     }
 
     if(events == GPIO_IRQ_EDGE_RISE){
         if(gpio == A_button){
-            button_pressed=1;
-            data d;
-            d.button = 1;
-            d.level = 0;
-            
-            xQueueSendFromISR(xQueueBTN,&d,0);
+            byteArray = 0b11111110 & byteArray;
         }
         if(gpio == S_button){
-            button_pressed=2;
-            data d;
-            d.button = 2;
-            d.level = 0;
-            
-            xQueueSendFromISR(xQueueBTN,&d,0);
+            byteArray = 0b11111101 & byteArray;
         }
         if(gpio == J_button){
-            button_pressed=3;
-            data d;
-            d.button = 3;
-            d.level = 0;
-            
-            xQueueSendFromISR(xQueueBTN,&d,0);
+            byteArray = 0b11111011 & byteArray;
         }
         if(gpio == K_button){
-            button_pressed=4;
-            data d;
-            d.button = 4;
-            d.level = 0;
-            
-            xQueueSendFromISR(xQueueBTN,&d,0);
+            byteArray = 0b11110111 & byteArray;
         }
         if(gpio == L_button){
-            button_pressed=5;
-            data d;
-            d.button = 5;
-            d.level = 0;
-            
-            xQueueSendFromISR(xQueueBTN,&d,0);
+            byteArray = 0b11101111 & byteArray;
         }
     }
 }
@@ -120,7 +66,6 @@ void btn_callback(uint gpio, uint32_t events){
 void y_task(void *p) {
     adc_init();
     adc_gpio_init(ADC_y);
-    int envia = 0;
     while(true) {
         adc_select_input(ADC_y_ID);
     
@@ -128,16 +73,15 @@ void y_task(void *p) {
         result -= 2047;
         result = result * 255 / 2047;
          
-        if ((result > 240 || result < -240) && envia == 0) {                 
-            xSemaphoreGive(xSemaphore_r);
-            envia = 1;   
+        if ((result > 240 || result < -240)) {                 
+            byteArray = 0b10000000 | byteArray;
         }
 
-        if ((result < 200 && result > -200) && envia == 1) {                 
-            envia = 0;  
+        if ((result < 200 && result > -200)) {                 
+            byteArray = 0b01111111 & byteArray;
         }
         
-        vTaskDelay(pdMS_TO_TICKS(100));
+        vTaskDelay(pdMS_TO_TICKS(1));
     }
 }
 
@@ -150,27 +94,29 @@ void sound_task(void *p) {
     
         int adc_value = adc_read();
         adc_value = adc_value * 100 / 4095;
-        
-        data d;
-        d.button = 6;
+
         if (adc_value < 10)
         {
-            d.level = -1;
+            byteArray = 0b00100000 | byteArray;
         }
         else if (adc_value > 90)
         {
-            d.level = 1;
+            byteArray = 0b01000000 | byteArray;
         }
         else
         {
-            d.button = 7;
-            d.level = 0;
+            byteArray = 0b11011111 & byteArray;
+            byteArray = 0b10111111 & byteArray;
         }
-        
-        xQueueSendFromISR(xQueueBTN, &d, 0);
-        
-        vTaskDelay(pdMS_TO_TICKS(100));
     }
+}
+
+bool timer_0_callback(repeating_timer_t *rt) {
+    char bytes = byteArray;
+    
+    xQueueSendFromISR(xQueueBTN, &bytes, 0);
+
+    return true; // keep repeating
 }
 
 void hc06_task(void *p) {
@@ -178,75 +124,26 @@ void hc06_task(void *p) {
     gpio_set_function(HC06_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(HC06_RX_PIN, GPIO_FUNC_UART);
     hc06_init("SelberEmbarcados", "1234");
-    data d;
+    char d;
+
+    repeating_timer_t timer_0;
+
+    if (!add_repeating_timer_us(40000, 
+                                timer_0_callback,
+                                NULL, 
+                                &timer_0)) {
+        printf("Failed to add timer\n");
+    }
     while (true) {
-        if (xQueueReceive(xQueueBTN,&d,pdMS_TO_TICKS(100))){
-            if(d.button==6){                    
-                uart_putc_raw(HC06_UART_ID, 'M');
-                uart_putc_raw(HC06_UART_ID, d.level);
-            }
-            
-            else if (d.level == 0)
-            {
-                if(d.button==1){                    
-                    uart_putc_raw(HC06_UART_ID, 'A');
-                    uart_putc_raw(HC06_UART_ID, d.level);
-
-                }
-                if(d.button==2){
-                    uart_putc_raw(HC06_UART_ID, 'S');
-                    uart_putc_raw(HC06_UART_ID, d.level);
-
-                }
-                if(d.button==3){                    
-                    uart_putc_raw(HC06_UART_ID, 'J');
-                    uart_putc_raw(HC06_UART_ID, d.level);
-
-                }
-                if(d.button==4){                    
-                    uart_putc_raw(HC06_UART_ID, 'K');
-                    uart_putc_raw(HC06_UART_ID, d.level);
-
-                }
-                if(d.button==5){                    
-                    uart_putc_raw(HC06_UART_ID, 'L');
-                    uart_putc_raw(HC06_UART_ID, d.level);
-                }
-            }
-            
-            else
-            {
-                if(xSemaphoreTake(xSemaphore_r, pdMS_TO_TICKS(500)) == pdTRUE){
-                    if(d.button==1){                    
-                        uart_putc_raw(HC06_UART_ID, 'A');
-                        uart_putc_raw(HC06_UART_ID, d.level);
-                    }
-                    if(d.button==2){
-                        uart_putc_raw(HC06_UART_ID, 'S');
-                        uart_putc_raw(HC06_UART_ID, d.level);
-                    }
-                    if(d.button==3){                    
-                        uart_putc_raw(HC06_UART_ID, 'J');
-                        uart_putc_raw(HC06_UART_ID, d.level);
-
-                    }
-                    if(d.button==4){                    
-                        uart_putc_raw(HC06_UART_ID, 'K');
-                        uart_putc_raw(HC06_UART_ID, d.level);
-                    }
-                    if(d.button==5){                    
-                        uart_putc_raw(HC06_UART_ID, 'L');
-                        uart_putc_raw(HC06_UART_ID, d.level);
-                    }
-                }
-            }
+        if (xQueueReceive(xQueueBTN,&d,pdMS_TO_TICKS(100))){ 
+            uart_putc_raw(HC06_UART_ID, d);
         }
         vTaskDelay(pdMS_TO_TICKS(1));
     }
 }
 int main() {
     stdio_init_all();
-    xQueueBTN = xQueueCreate(32, sizeof(data));
+    xQueueBTN = xQueueCreate(32, sizeof(char));
     gpio_init(A_button);
     gpio_set_dir(A_button,GPIO_IN);
     gpio_pull_up(A_button);
--- python/main.py
+++ python/main.py
@@ -10,48 +10,39 @@ device = uinput.Device([
     uinput.KEY_S,
     uinput.KEY_J,
     uinput.KEY_VOLUMEUP,
-    uinput.KEY_VOLUMEDOWN
+    uinput.KEY_VOLUMEDOWN,
+    uinput.KEY_UP
 ])
 
+button = [uinput.KEY_UP, uinput.KEY_VOLUMEDOWN, uinput.KEY_VOLUMEUP, uinput.KEY_L, uinput.KEY_K, uinput.KEY_J, uinput.KEY_S, uinput.KEY_A]
+
+def byte_para_bits(byte):
+    # Converte o byte para um valor inteiro
+    byte_int = int.from_bytes(byte, byteorder='big')
+
+    # Inicializa uma lista vazia para armazenar os bits
+    bits = []
+
+    # Itera sobre cada bit no byte, da posição mais significativa para a menos significativa
+    for i in range(7, -1, -1):
+        # Verifica se o bit na posição i está definido (1) ou não (0)
+        bit = (byte_int >> i) & 1
+        # Adiciona o bit à lista de bits
+        bits.append(bit)
+
+    return bits
 
 try:
     # sync package
     while True:
         print('Waiting for sync package...')
         while True:
-            data = ser.read(2)
-            print(ascii(data))
-            button = data[0]
-            print(chr(button))
-            up_down = data[1]
-            print(up_down)
-            if chr(button) == 'A':
-                print("a chegou")
-                #device.emit_click(uinput.KEY_A)
-                device.emit(uinput.KEY_A, up_down)
-            if chr(button) == 'K':
-                print("K chegou")
-                #device.emit_click(uinput.KEY_K)
-                device.emit(uinput.KEY_K, up_down)
-            if chr(button) == 'J':
-                print("J chegou")
-                #device.emit_click(uinput.KEY_J)
-                device.emit(uinput.KEY_J, up_down)
-            if chr(button) == 'L':
-                print("L chegou")
-                #device.emit_click(uinput.KEY_L)
-                device.emit(uinput.KEY_L, up_down)
-            if chr(button) == 'S':
-                print("S chegou")
-                #device.emit_click(uinput.KEY_S)
-                device.emit(uinput.KEY_S, up_down)
-            if chr(button) == 'M':
-                print("M chegou")
-                #device.emit_click(uinput.KEY_S)
-                if (up_down == 255):
-                    device.emit_click(uinput.KEY_VOLUMEDOWN)
-                if (up_down == 1):
-                    device.emit_click(uinput.KEY_VOLUMEUP)
+            data = ser.read(1)
+            bits = byte_para_bits(data)
+            i = 0
+            while (i < 8):
+                device.emit(button[i], bits[i])
+                i += 1
 
 except KeyboardInterrupt:
     print("Program terminated by user")
